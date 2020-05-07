a41daf0f652c326de4eb243d564cbf680befa55e
Myronov Denys <myronovdenys@gmail.com>
2020-05-07 17:50:44 +0300

    Fix formatting

--- src/test/groovy/apitest/CocktailsSpecification.groovy
+++ src/test/groovy/apitest/CocktailsSpecification.groovy
@@ -7,17 +7,16 @@ import spock.lang.Unroll
 /**
  * Created by demyr on 10-Nov-19.
  */
-class CocktailsSpecification extends BaseSpecification{
-
-    def setupSpec(){
+class CocktailsSpecification extends BaseSpecification {
+    def setupSpec() {
         // Connect to DB and and add predefined objects
     }
-    def cleanupSpec(){
+
+    def cleanupSpec() {
         // Delete objects from DB
     }
 
     def "Each cocktail should has instructions how to mix"() {
-
         when:
         def cocktailsIds = httpHelper().get("/filter.php?c=Cocktail").getBodyAsJson().drinks.idDrink
 
@@ -29,7 +28,6 @@ class CocktailsSpecification extends BaseSpecification{
 
     @Unroll
     def "Should be able to filter drinks by certain characteristics"() {
-
         when:
         def response = httpHelper().get("/filter.php?$characteristic=$value")
 
@@ -43,24 +41,23 @@ class CocktailsSpecification extends BaseSpecification{
                         assert it.strDrink
                         assert it.strDrinkThumb
                     }
-                 }
+                }
         where:
-        characteristic | value              | drinksCount
-        "i"            | "vodka"            | 83
-        "i"            | "gin"              | 95
-        "c"            | "Ordinary_Drink"   | 100
-        "c"            | "Cocktail"         | 100
-        "g"            | "Cocktail_glass"   | 100
-        "g"            | "Champagne_flute"  | 13
-        "a"            | "Alcoholic"        | 100
-        "a"            | "Non_Alcoholic"    | 58
+        characteristic | value             | drinksCount
+        "i"            | "vodka"           | 83
+        "i"            | "gin"             | 95
+        "c"            | "Ordinary_Drink"  | 100
+        "c"            | "Cocktail"        | 100
+        "g"            | "Cocktail_glass"  | 100
+        "g"            | "Champagne_flute" | 13
+        "a"            | "Alcoholic"       | 100
+        "a"            | "Non_Alcoholic"   | 58
     }
 
     def "Should be able to search cocktail by name and open it details"() {
-
         given:
         String cocktailName = "Blue Margarita"
-        def cocktailId = httpHelper().get("/search.php?s=$cocktailName").getBodyAsJson().drinks.idDrink.toString().replaceAll("[\\[\\]]","")
+        def cocktailId = httpHelper().get("/search.php?s=$cocktailName").getBodyAsJson().drinks.idDrink.toString().replaceAll("[\\[\\]]", "")
 
         when:
         def response = httpHelper().get("/lookup.php?i=$cocktailId")
@@ -68,8 +65,8 @@ class CocktailsSpecification extends BaseSpecification{
         then:
         response
                 .withBody {
-                    assert it.drinks.idDrink.toString().replaceAll("[\\[\\]]","") == cocktailId
-                    assert it.drinks.strDrink.toString().replaceAll("[\\[\\]]","") == cocktailName
+                    assert it.drinks.idDrink.toString().replaceAll("[\\[\\]]", "") == cocktailId
+                    assert it.drinks.strDrink.toString().replaceAll("[\\[\\]]", "") == cocktailName
                     assert it.drinks.strAlcoholic
                     assert it.drinks.strCategory
                     assert it.drinks.strGlass
@@ -78,7 +75,6 @@ class CocktailsSpecification extends BaseSpecification{
     }
 
     def "Should result empty list by filtering non existing ingredient"() {
-
         when:
         def response = httpHelper().get("/filter.php?i=NotExistingIngredient")
 
